38ec1661d8da234a064d2142626514531cff64a2
nhuesman1043 <noah.huesman@gmail.com>
2025-01-29 12:57:24 -0600

    feat: initial about section and some icons

--- src/app/(public)/components/about/about.tsx
+++ src/app/(public)/components/about/about.tsx
@@ -46,40 +46,26 @@ export function About() {
 						<Title>About</Title>
 						<Grid gutter={{ base: "lg", md: 100 }}>
 							<GridCol span={{ base: 12, sm: 6, md: 7 }}>
-								<Text className={classes.text}>
-									A clear example of a technological
-									innovation where an analysis based on
-									Rawls&apos; veil of ignorance exposes both
-									benefits and harms is the widespread
-									adoption of automation and artificial
-									intelligence (AI) in the workforce. If we
-									examine this innovation behind the veil of
-									ignorance, we must imagine ourselves not
-									knowing whether we are a highly skilled
-									professional, a low-skilled laborer, or
-									someone lacking the education or resources
-									to adapt to the changing job market. From
-									this impartial standpoint, it becomes clear
-									that while automation can bring significant
-									benefits, it also disproportionately harms
-									certain groups in society. On the one hand,
-									automation and AI can greatly benefit
-									high-skilled workers in sectors like
-									technology, software development, and AI
-									management, where increased efficiency and
-									productivity can lead to new job
-									opportunities and higher wages. The use of
-									AI in industries like healthcare or finance
-									can also lead to better services, reduced
-									costs, and innovation, contributing to
-									overall societal progress.
+								<Text size="lg" className={classes.text}>
+									Driven by a passion for problem-solving and
+									efficiency, I&apos;m a software engineer
+									with experience in automation, cloud
+									development, and system optimization. I
+									thrive on building high-impact solutions,
+									whether it&apos;s optimizing server
+									performance, streamlining CI/CD pipelines,
+									or developing scalable applications. My work
+									spans from hands-on development at Signum
+									and Marvin to leading technical efforts in
+									student organizations and my capstone
+									project for Galois.
 								</Text>
 							</GridCol>
 							<GridCol span={{ base: 12, sm: 6, md: 5 }}>
 								<Image
 									component={NextImage}
 									src="/images/headshot.jpg"
-									alt="Noah Huesman"
+									alt="My headshot"
 									width={5000}
 									height={5000}
 									className={classes.image}
--- src/app/(public)/components/footer/footer.tsx
+++ src/app/(public)/components/footer/footer.tsx
@@ -5,6 +5,7 @@ Author: Noah Huesman
 Creation Date: 01/25/2025
 Modification History:
 #1 (01/25/2025) - Initial creation - Noah Huesman
+#2 (01/28/2025) - Added social media links - Noah Huesman
 ================================================================ */
 
 // ========================================
@@ -14,24 +15,72 @@ Modification History:
 // CSS
 // import classes from "./footer.module.css"
 
-// Components
+// Shared components
+import { TransitionLink } from "@/components/shared"
 
 // Mantine
-import { Container } from "@mantine/core"
+import {
+	Container,
+	Group,
+	HoverCard,
+	HoverCardTarget,
+	HoverCardDropdown,
+} from "@mantine/core"
 
 // Theme
-// import { theme } from "@/app/theme"
+import { theme } from "@/app/theme"
+
+// Tabler icons
+import { IconBrandLinkedin, IconBrandGithub } from "@tabler/icons-react"
+
+// Types library
+import { SocialMedia } from "@/library/types"
 
 // ========================================
 // FOOTER
 // ========================================
 
 export function Footer() {
+	// Define social media and their attributes
+	const socialMedia: Record<string, SocialMedia> = {
+		linkedin: {
+			title: "LinkedIn",
+			link: "https://www.linkedin.com/in/noah-huesman-6272121bb/",
+			icon: <IconBrandLinkedin stroke={1.1} size={35} />,
+		},
+		github: {
+			title: "GitHub",
+			link: "https://github.com/nhuesman1043?tab=repositories",
+			icon: <IconBrandGithub stroke={1.1} size={35} />,
+		},
+	}
+
 	// Render
 	return (
 		<div id="footer" className="z-50">
-			<Container fluid className="border-2 border-purple-600">
-				footer
+			<Container fluid bg={theme.colors.backgroundPrimary[0]} h={70}>
+				<Group justify="center" h="100%" gap="lg">
+					{Object.values(socialMedia).map((medium: SocialMedia) => {
+						return (
+							<HoverCard
+								withArrow
+								arrowPosition="center"
+								arrowSize={10}
+								closeDelay={-100}
+								key={medium.title}
+							>
+								<HoverCardTarget>
+									<TransitionLink href={medium.link}>
+										{medium.icon}
+									</TransitionLink>
+								</HoverCardTarget>
+								<HoverCardDropdown>
+									{medium.title}
+								</HoverCardDropdown>
+							</HoverCard>
+						)
+					})}
+				</Group>
 			</Container>
 		</div>
 	)
